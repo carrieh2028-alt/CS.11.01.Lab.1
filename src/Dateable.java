af45075f920d06fd3328a8a9a33293643786bd82
carriehe lab 1 <carrieh2028@student.cis.edu.hk>
2025-09-16 14:37:20 +0800

    Carrie He Lab 1 Submission

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner yourAge = new Scanner(System.in);
+
+        System.out.println("Enter your age using numbers: ");
+        int age = yourAge.nextInt();
+        double age1 = ((double)age / 2) + 7;
+        System.out.println("Your should date someone who is at least " + age1 + " years old.");
+
+        yourAge.close();
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
 
+        Scanner yourAge = new Scanner(System.in);
+
+        System.out.println("Enter your age using numbers: ");
+        int age = yourAge.nextInt();
+
+        int ageA = 100 - age;
+        System.out.println("You should read at least " + ageA + " pages before giving up a book.");
+
+        yourAge.close();
     }
 }
+
